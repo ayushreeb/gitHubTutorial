1294e8eae4383e64585e57fb85ac1d0b4359dfb8
Ayushree Beura <ariya.beura@gmail.com>
2024-07-12 11:59:59 -0700

    Initial Commit

---
+++ gitHubTutorial/gitHubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubTutorialApp.swift
+//  gitHubTutorial
+//
+//  Created by Ayushree Beura on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct gitHubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
