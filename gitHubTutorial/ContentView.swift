6a9727e40272e3adf905a6efb72facf88f07c04d
Ayushree Beura <ariya.beura@gmail.com>
2024-07-16 13:07:04 -0700

    changed text

--- gitHubTutorial/ContentView.swift
+++ gitHubTutorial/ContentView.swift
@@ -5,20 +5,61 @@
 //  Created by Ayushree Beura on 7/12/24.
 //
 
+// Log in page (When you first open the app)
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var TextTitle = "What is your name?"
+    @State private var name = ""
+    @State private var showAlert = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(alignment: .center) {
+            
+            HStack {
+                
+                Text("Unwind")
+                    .font(.system(size: 70, weight: .black, design: .serif))
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom)
+            }
         }
-        .padding()
+        VStack {
+            
+            NavigationStack {
+                VStack{
+                    NavigationLink(destination: nextPage()) {
+                        Text("➡️")
+                        
+                            .navigationTitle("Home")
+                            .navigationBarTitleDisplayMode(.inline)
+                            .navigationBarHidden(true)
+                    }
+                    Text(TextTitle)
+                        .font(.system(size: 30, weight: .black, design: .serif))
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color.black)
+                    
+                    TextField("", text: $name)
+                        .multilineTextAlignment(.center)
+                        .font(.title)
+                        .border(Color.gray, width: 1)
+                    
+                    Button("Submit Name")
+                    {
+                        TextTitle = "Welcome, \(name)!✨"
+                    }
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(Color.pink)
+                }
+                .padding()
+            }
+        }
+        
     }
-}
-
-#Preview {
-    ContentView()
 }
---
+++ nextPage.swift
@@ -0,0 +1,18 @@
+//
+//  nextPage.swift
+//  gitHubTutorial
+//
+//  Created by Ayushree Beura on 7/17/24.
+//
+
+import SwiftUI
+
+struct nextPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    nextPage()
+}
